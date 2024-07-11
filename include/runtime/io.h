450042cdb8998f7090aa7df85410cc0ab9e50c8c
AvirukBasak <avirukbasak@gmail.com>
2024-07-11 22:51:21 +0530

    add optional AddressSanitizer custom error handler function

--- include/runtime/io.h
+++ include/runtime/io.h
@@ -1,6 +1,8 @@
 #ifndef RT_IO_H
 #define RT_IO_H
 
+void __asan_on_error();
+
 void rt_throw_exitcode(int exitcode, const char *fmt, ...) __attribute__((format(printf, 2, 3)));
 void rt_throw(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
 
